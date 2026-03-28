5983f954568e6a184501c347bdd61d12786f8a4a
Sapan Desai <sapan022006@gmail.com>
2026-03-28 22:25:12 +0530

    Configure custom subdomain coach.sapandesai.me

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // Root path for subdomain
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
